ad683f3bf377c4500b88cf3b0f15708793c72a74
alow02 <alexlowv@gmail.com>
2021-07-26 15:10:27 -0700

    Project_functions.py created and updated

---
+++ analysis/scripts/project_functions.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+
+def load_and_process():
+
+    # Chain 1: import the data and remove unwanted columns
+    df1 = (
+        pd.read_csv("../raw/allCetaceanData.csv")
+        .drop('mother',axis='columns')
+        .drop('father',axis='columns')
+        .drop('region',axis='columns')
+        .drop('transfer',axis='columns')
+        .drop('transferDate',axis='columns')
+        .drop('notes', axis = 'columns')
+    )
+    
+    # Chain 2: change columns to datetime, remove timestamps, create timespent calculated column
+    df2 = df.assign(
+        originDate=lambda x: pd.to_datetime(x['originDate']).dt.date,
+        statusDate=lambda x: pd.to_datetime(x['statusDate']).dt.date,
+        timespent=lambda x: x['statusDate'] - x['originDate']
+    )
+    
+    # Chain 3: change birthdate accuracy value labels
+ 
+    df3 = (
+        df.replace('a','actual')
+        .replace('e','estimate')
+        .replace('u','unknown')
+    )
+    
+    return df3
